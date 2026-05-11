deff9a642dfaea2f428d934728baf8731905a697
goatshuman <anshumanbahuguna94@gmail.com>
2026-05-11 21:48:21 +0530

    Remove first-time attention modal on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./layout/Header";
-import FirstTimeModal from "./components/Model/firstTimeModel";
+  import Home from "./pages/Home";
+  import Header from "./layout/Header";
 
-function App() {
-  const base = import.meta.env.BASE_URL.replace(/\/$/, "");
-  return (
-    <BrowserRouter basename={base}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <Home />
-            </>
-          }
-        />
-      </Routes>
-      <FirstTimeModal />
-    </BrowserRouter>
-  );
-}
+  function App() {
+    const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+    return (
+      <BrowserRouter basename={base}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 
-export default App;
+  export default App;
+  
